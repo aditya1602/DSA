4bd6f8b9533408e9b3230d4fd1054e98489b39ca
aditya1602 <aditya.p.sailesh@gmail.com>
2023-04-18 23:49:34 +0530

    Conditional Statements Questions Added

---
+++ Conditional/Is_prime.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=2;
+    
+    while (i<n)
+    {
+        if (n%i==0)
+        {
+            cout<<"not a prime"<<endl;
+            break;
+        }
+        else{
+            i++;
+        }
+        
+    }
+    
+}
